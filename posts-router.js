18a578b6177e27d5ed52d824ddd29b25ac5ea85d
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-05-10 15:44:25 -0700

    Added PUT endpoint

--- posts-router.js
+++ posts-router.js
@@ -62,7 +62,21 @@ router.delete('/:id', async (req, res) => {
 })
 
 // PUT
-
-
+router.put('/:id', async (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    try {
+        const post = await Posts.update(req.params.id, req.body);
+        if(!req.body.title || !req.body.contents) {
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+        } else if (post) {
+            res.status(200).json({ "id": req.params.id, ...req.body});
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "The post information could not be modified." });
+    }
+})
 
 module.exports = router;
