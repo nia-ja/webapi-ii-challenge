9d8b8ccc49ce9b50fdbcb6cc2be5f158422a8402
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-05-10 12:37:16 -0700

    Added POST endpoint

--- posts-router.js
+++ posts-router.js
@@ -21,18 +21,30 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         const post = await Posts.findById(req.params.id);
-        console.log(post);
         if (post.length) {
             res.status(200).json(post);
         } else {
             res.status(404).json({ message: "The post with the specified ID does not exist." })
         }
     } catch (error) {
-        res.status(500).json({ error: "The post information could not be retrieved." })
+        res.status(500).json({ error: "The post information could not be retrieved." });
     }
 })
 
 // POST
+router.post('/', async (req, res) => {
+    try {
+        const post = await Posts.insert(req.body);
+        if (!req.body.title || !req.body.contents) {
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+        } else {
+            res.status(201).json(post);
+        }
+
+    } catch (error) {
+        res.status(500).json({ error: "There was an error while saving the post to the database" });
+    }
+})
 
 // DELETE
 
